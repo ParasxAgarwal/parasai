88f14bf033c9ca7de9b386ed43c26a6023811bef
ParasxAgarwal <Paras.ganesha@gmail.com>
2025-09-26 16:59:37 +0530

    debug -prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
