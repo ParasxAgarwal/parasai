c450a42bf6d9981c28dc2f19d62ca99e4f7781a6
ParasxAgarwal <Paras.ganesha@gmail.com>
2025-09-26 16:53:05 +0530

    better_auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { env } from 'node:process'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div className="text-yellow-600">Home</div>
+    <div className="text-yellow-600">{env.BETTER_AUTH_URL}</div>
   )
 }
